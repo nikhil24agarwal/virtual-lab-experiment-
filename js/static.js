c8fdcdea9a726b18297456dae00dc0f57265d6ec
unknown <nikhil2386@yahoo.com>
2020-05-31 15:59:58 +0530

    canvas initial commit

--- js/static.js
+++ js/static.js
@@ -1,3 +1,54 @@
+var canvas = document.querySelector("canvas");
+var c=canvas.getContext("2d");
+var x1=130
+,y1=170
+,x2=375
+,y2=50
+,x3=620
+,y3=170
+,x4=252.5
+,y4=370
+,x5=497.5
+,y5=370
+c.lineWidth = 3;
+
+
+c.beginPath();
+c.arc(x1,y1,30,0,Math.PI*2,false);
+c.font= "20px serif"
+c.fillText("A",x1-7,y1+5);
+c.stroke();
+
+c.beginPath();
+c.arc(x2,y2,30,0,Math.PI*2,false);
+c.font= "20px serif"
+c.fillText("B",x2-7,y2+5);
+c.stroke();
+
+c.beginPath();
+c.arc(x3,y3,30,0,Math.PI*2,false);
+c.font= "20px serif"
+c.fillText("C",x3-7,y3+5);
+c.stroke();
+
+c.beginPath();
+c.arc(x4,y4,30,0,Math.PI*2,false);
+c.font= "20px serif"
+c.fillText("D",x4-7,y4+5);
+c.stroke();
+
+c.beginPath();
+c.arc(x5,y5,30,0,Math.PI*2,false);
+c.font= "20px serif"
+c.fillText("E",x5-7,y5+5);
+c.stroke();
+
+function tablee(arr1){
+    
+}
+
+
+
 function f(){
     var atable={
         dist:[],
@@ -39,6 +90,7 @@ function f(){
         etable.dist.push(document.querySelector("body > table:nth-child(5) > tbody > tr:nth-child(" + i +")" +"> td:nth-child(2) > input").value);
         etable.nextn.push(document.querySelector("body > table:nth-child(5) > tbody > tr:nth-child(" + i +")" +"> td:nth-child(3) > input").value);
     }
-    console.log(atable.dist);
+
+    
     
 }
